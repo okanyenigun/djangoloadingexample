d3bbe3da542d85eec9df907c6c70d3eef4f685f4
okanyenigun <okanyenigun@gmail.com>
2022-04-05 16:20:17 +0300

    first commit

---
+++ ml/service.py
@@ -0,0 +1,39 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import Lasso
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+
+class Regression:
+    
+    def __init__(self):
+        #generate dataset
+        X, y = self._generate_dataset()
+        #run model
+        self.rmse = self._run_model(X,y)
+        
+    def get_rmse(self):
+        return self.rmse
+
+    def _generate_dataset(self):
+        X,y = datasets.make_regression(n_samples=1000000,#number of samples
+                                      n_features=15,#number of features
+                                      n_informative=10,#number of useful features 
+                                      noise=10,#bias and standard deviation of the guassian noise
+                                      random_state=0) #set for same data points for each run
+        return X,y
+
+    def _run_model(self,X,y):
+        X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3)
+        model = Lasso()
+        param_grid = {
+            "max_iter": [100,1000,10000],
+            "alpha": [0.00001,0.0001,0.001]
+        }
+        cv = GridSearchCV(model,param_grid=param_grid,n_jobs=-1,cv=5,scoring='neg_mean_absolute_error')
+        cv.fit(X_train,y_train)
+        best_model = cv.best_estimator_
+        y_pred = best_model.predict(X_test)
+        rms = sqrt(mean_squared_error(y_test,y_pred))
+        return rms
---
+++ ml/views.py
@@ -0,0 +1,32 @@
+import time
+import json
+from .service import Regression
+from django.shortcuts import render,redirect
+from django.views import View
+from django.http import HttpResponse
+# Create your views here.
+
+class MainView(View):
+    
+    def get(self,request):
+        if "submit" in request.GET:
+            print("here")
+            return redirect("result")
+        return render(request,'./templates/index.html')
+
+
+class ResultView(View):
+    
+    def get(self,request):
+        print("isndei")
+        return render(request,'./templates/results.html')
+
+    
+    @staticmethod
+    def ajax_view(request):
+        start_time = time.time()
+        R = Regression()
+        rmse = R.get_rmse()
+        context = {"rmse":rmse,"time":time.time()-start_time}
+        data = json.dumps(context)
+        return HttpResponse(data,content_type="application/json")
